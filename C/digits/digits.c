a9937f9f6ad05588a0ded3a77e675debe36cc4de
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-07 23:10:31 +0200

    Program prints the amount of digits in a number.

---
+++ C/digits/digits.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the amount of digits in a number
+ *
+ * Description: Takes a positive integer, divides it by 10
+ *				until it has one digit left.
+ *				The amount of times we divide the number is
+ *				is the amount of digits in that number.
+ *
+ * Return: On success - (0)
+ */
+
+int main(void)
+{
+	int digits = 0, num, a;
+
+	a = num = get_int("Enter a number from 0: ");
+
+	do {
+		num /= 10;
+		digits++;
+	} while (num > 0);
+
+	printf("The number %d has %d digits.\n", a, digits);
+
+	return (0);
+}
