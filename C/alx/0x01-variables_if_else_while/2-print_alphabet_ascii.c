1b37e02109e25626cdc4744314979aef19fcb5d0
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-13 17:11:03 +0200

    Prints the alphabet in lower case using putchar.

---
+++ C/alx/0x01-variables_if_else_while/2-print_alphabet_ascii.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lower case using putchar
+ * 			and ASCII values.
+ *
+ * Return: On success - (0)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 122; i++)
+	{
+		putchar(i);
+	}
+	printf("\n");
+
+	return (0);
+}
