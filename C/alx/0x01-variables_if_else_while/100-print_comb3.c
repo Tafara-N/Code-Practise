2f2d814867309fb9c35aaeddd109ab734499574a
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-13 19:11:19 +0200

    Program that prints all possible different combinations of two digits

---
+++ C/alx/0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints all possible different combinations of two digits
+ *
+ * Return: On success - (0)
+ */
+
+int main(void)
+{
+	return (0);
+}
