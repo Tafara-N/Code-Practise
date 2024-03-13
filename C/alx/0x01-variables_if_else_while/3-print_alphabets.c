349ffbc47d9e67e786356d42b302b29578c5a970
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-13 16:46:40 +0200

    Prints the alphabet in lower case then in UPPERCASE using putchar.

---
+++ C/alx/0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <ctype.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lower case then in UPPERCASE using putchar
+ *
+ * Return: On success - (0)
+ */
+
+int main(void)
+{
+	int i = 0;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	for (i = 0; i < 26; i++)
+	{
+		putchar(toupper(alphabet[i]));
+	}
+
+	printf("\n");
+
+	return (0);
+}
