acc3c830a52259569286e23fe85c796952491a1e
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-13 16:31:27 +0200

    Prints last digit of a number and whether it is greater than 5, less than 6, or 0.

--- C/alx/0x01-variables_if_else_while/1-last_digit.c
+++ C/alx/0x01-variables_if_else_while/1-last_digit.c
@@ -6,7 +6,7 @@
  * main - Prints the last digit of a randomly generated number
  *			and whether it is greater than 5, less than 6, or 0.
  *
- * Returns: On success - (0)
+ * Return: On success - (0)
  */
 
 int main(void)
