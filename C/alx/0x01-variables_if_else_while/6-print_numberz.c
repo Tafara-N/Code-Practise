3e701fbc12eb8aafac1f9ced991286a8cafd6bdd
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-13 17:32:22 +0200

    Prints all single digit numbers of base 10 starting from 0.

---
+++ C/alx/0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: On success - (0)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	printf("\n");
+
+	return (0);
+}
