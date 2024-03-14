00eef171b688a9e098c7a761ebf9b493e539a36b
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-14 17:00:29 +0200

    Header file.

---
+++ C/alx/0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);  /* Function we're supposed to use */
+
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+
+#endif /* MAIN_H */
