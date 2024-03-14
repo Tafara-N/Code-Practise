4db61d98c5ea44b4b4807baa83aebca58029c7b1
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-14 14:32:05 +0200

    Program prints _putchar.

--- C/alx/0x02-functions_nested_loops/0-putchar.c
+++ C/alx/0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Program prints _putchar
@@ -8,7 +8,15 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
