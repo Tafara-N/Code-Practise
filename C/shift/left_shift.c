88a76f362472aec32e0ffbb6606ccf770640dc9e
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-16 00:43:55 +0200

    Function left shifts bits of a number by n times.

---
+++ C/shift/left_shift.c
@@ -0,0 +1,15 @@
+#include "shift.h"
+
+/**
+ * left_shift - Function right shifts bits of a number by n times
+ * @number: Number whose bits are to be shifted
+ * @times: Number of times to shift the bits
+ *
+ * Return: Number
+*/
+
+int left_shift(int number, int times)
+{
+	number <<= times;
+	return number;
+}
