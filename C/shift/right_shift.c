3e327e41e1c242f2795c9dfcaf825c5ecf6e82a6
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-16 00:43:33 +0200

    Function right shifts bits of a number by n times.

---
+++ C/shift/right_shift.c
@@ -0,0 +1,15 @@
+#include "shift.h"
+
+/**
+ * right_shift - Function right shifts bits of a number by n times
+ * @number: Number whose bits are to be shifted
+ * @times: Number of times to shift the bits
+ *
+ * Return: Number
+*/
+
+int right_shift(long number, int times)
+{
+	number >>= times;
+	return number;
+}
