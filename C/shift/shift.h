2aa282d58cd9e2616591c48bd547f875e3bc7ec2
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-16 01:16:56 +0200

    Header file.

---
+++ C/shift/shift.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+
+int right_shift(long number, int times);
+int left_shift(long number, int times);
+
+int right_count(long number, int times);
+int left_count(long number, int times);
+
+#endif
