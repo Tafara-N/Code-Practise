01a67cdc7f9afffcf098ab7941f2350f5235bda8
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-19 21:52:30 +0200

    Testing the test_loop function.

---
+++ C/functions/test_loop.c
@@ -0,0 +1,13 @@
+/**
+ * test_loop - Testing the test_loop function
+ *
+ * Return: On success - (decrement the number)
+*/
+
+
+int test_loop(void)
+{
+	static int number = 16;
+
+	return (number--);
+}
