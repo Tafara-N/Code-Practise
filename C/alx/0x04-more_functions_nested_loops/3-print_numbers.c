d3e22ac89b61330e9bcbdb195a1662df240c0d34
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-20 21:00:52 +0200

    Function prints numbers (0 through 9).

---
+++ C/alx/0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers: Function prints numbers, from 0 to 9
+ *
+ * Return: Printed numbers
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
