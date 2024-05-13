7960d34798c486a068f38a90697bc28adc0536f9
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-05-13 11:53:33 +0200

    Print the integer stored in the variable number, followed by Battery street.

--- Python/alx/0x00-python-hello_world/3-print_number.py
+++ Python/alx/0x00-python-hello_world/3-print_number.py
@@ -1,11 +1,3 @@
 #!/usr/bin/env python3
-
-"""
-Print the integer stored in the variable number, followed by Battery street
-You are not allowed to cast the variable number into a string
-Your code must be 3 lines long
-"""
-
 number = 98
-
 print(f"{number} Battery street")
