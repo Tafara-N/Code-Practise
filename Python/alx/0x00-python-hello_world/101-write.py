3d481bedd3cb8bd22bf88ffe83a1e791c0a234d8
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-05-13 15:39:30 +0200

    Prints exactly, and that piece of art is useful - Dora Korpar, 2015-10-19

---
+++ Python/alx/0x00-python-hello_world/101-write.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Prints exactly, and that piece of art is useful - Dora Korpar, 2015-10-19
+import sys
+
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.exit(1)
