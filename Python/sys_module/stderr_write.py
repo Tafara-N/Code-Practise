1e6400e0c2e91f4a0f0e08d0b4881595a7b8c153
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-05-13 15:37:17 +0200

    Prints exactly, and that piece of art is useful - Dora Korpar, 2015-10-19

---
+++ Python/sys_module/stderr_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Prints exactly, and that piece of art is useful - Dora Korpar, 2015-10-19
+import sys
+from sys import stderr, stdout
+
+stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+stdout.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.exit(1)
