65cb1108e2a70d5734b1c69a4558a132a667cc5b
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-09-29 01:02:59 +0200

    Make the `cheesy` element of `things` all uppercase and then print the list.

---
+++ Python/Books/Introducing Python/chapter-3/uppercase_cheesy_element.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Make the `cheesy` element of `things` all uppercase and then print the list
+"""
+
+things = ["mozzarella", "cinderella", "salmonella"]
+
+things[0] = things[0].upper()
+
+print(things)
