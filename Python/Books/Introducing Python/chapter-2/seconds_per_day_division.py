cdd05f5bd9f5d2fa2eb278d4cb8461a9adb622f9
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-09-29 00:51:54 +0200

    Performing floating-point and integer division on the number of seconds per day.

--- Python/Books/Introducing Python/chapter-2/seconds_per_day_division.py
+++ Python/Books/Introducing Python/chapter-2/seconds_per_day_division.py
@@ -8,8 +8,8 @@ and store the results in the variables `seconds_per_day_float` and `seconds_per_
 seconds_per_hour = 60 * 60
 seconds_per_day = seconds_per_hour * 24
 
-seconds_per_day_float = seconds_per_day / 1.0
-seconds_per_day_int = seconds_per_day // 1
+division_float = seconds_per_hour / seconds_per_day
+division_int = seconds_per_hour // seconds_per_day
 
-print("Float:", seconds_per_day_float)
-print("Integer:", seconds_per_day_int)
+print("Float:", division_float)
+print("Integer:", division_int)
