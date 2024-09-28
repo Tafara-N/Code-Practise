3bedd5a41aa9d9bbd82ed3624f8a901dac72dbfa
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-09-29 01:50:43 +0200

    Using `f2e`, print the English equivalent of the French word `chien`.

---
+++ Python/Books/Introducing Python/chapter-3/print_chien.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Using `f2e`, print the English equivalent of the French word `chien`
+"""
+
+from rich import print_json
+
+
+f2e: dict[str, str] = {
+    "chien": "dog",
+    "chat": "cat",
+    "morse": "walrus",
+}
+
+print_json(data=f2e["chien"])
