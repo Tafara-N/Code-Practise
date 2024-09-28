3786ea193a67edcf1f16ca3da10897a3bcee3d7e
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-09-29 01:09:17 +0200

    Delete the disease element from the list.

---
+++ Python/Books/Introducing Python/chapter-3/delete_disease_element.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Delete the `disease` element from `things`, collect your Nobel Prize, and
+print the list
+
+"""
+
+things = ["mozzarella", "cinderella", "salmonella"]
+
+del things[2]
+# things.pop(-1)
+# things.pop(2)
+# things.remove("salmonella")
+
+print(things)
