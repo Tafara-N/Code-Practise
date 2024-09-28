3db573b31639b1ed16aa492b39e8f4203ef92cab
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-09-29 01:46:31 +0200

    A French-to-English dictionary called 'f2e'.

--- Python/Books/Introducing Python/chapter-3/f2e.py
+++ Python/Books/Introducing Python/chapter-3/f2e.py
@@ -5,6 +5,8 @@ Make a French-to-English dictionary called `f2e` from `e2f`.
 
 Use the `items` method
 
+------------
+
 In the `f2e` dictionary, keys are French words and values are English words.
 
 The items method returns a list of tuples, where each tuple is a key-value pair.
