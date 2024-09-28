fc13cb17162a2b9f5ef99cd3efc011d33bae7ebe
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-09-29 00:54:16 +0200

    A list called things with three elements.

---
+++ Python/Books/Introducing Python/chapter-3/things.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Make a list called `things` with these three strings as elements:
+"mozzarella", "cinderella", "salmonella"
+
+"""
+
+things = ["mozzarella", "cinderella", "salmonella"]
+
+print(things)
