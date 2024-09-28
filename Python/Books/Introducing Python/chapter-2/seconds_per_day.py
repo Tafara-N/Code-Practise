6c293a15528e7c83b87425e930735dc0ae8b991d
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-09-28 23:47:00 +0200

    Calculating the number of seconds in a day and store the result in a variable.

---
+++ Python/Books/Introducing Python/chapter-2/seconds_per_day.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Calculate the number of seconds per day and store the result in the variable
+`seconds_per_hour`
+
+"""
+
+seconds_per_hour = 60 * 60
+seconds_per_day = seconds_per_hour * 24
+
+print(seconds_per_day)
