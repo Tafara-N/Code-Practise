18ed485dd365a83328269bfbd31210cef7fad950
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-09-29 00:38:39 +0200

    A list called 'years_list', starting with the year of your birth, and each year thereafter until the year of your fifth birthday.

---
+++ Python/Books/Introducing Python/chapter-3/years_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+A list called `years_list`, starting with the year of your birth, and each year
+thereafter until the year of your fifth birthday.
+
+"""
+
+years_list = [1997, 1998, 1999, 2000, 2001, 2002]
+
+print(years_list)
