54282d9921ac39414d4f44dd1d975510881fc805
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-09-28 23:45:19 +0200

    Calculating the number of seconds in an hour and store the result in a variable.

---
+++ Python/Books/Introducing Python/chapter-2/seconds_per_hour.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Calculate the number of seconds in an hour and store the result in the variable
+`seconds_per_hour`
+
+"""
+
+seconds_per_hour = 60 * 60
+
+print(seconds_per_hour)
