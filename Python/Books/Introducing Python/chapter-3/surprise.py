11d81f3a5e5fe0ee68bfdc2cd78e2a581103c9bb
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-09-29 01:11:17 +0200

    A list called 'surprise' with three elements.

---
+++ Python/Books/Introducing Python/chapter-3/surprise.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Create a list called `surprise` with the elements:
+"Groucho", "Chico", and "Harpo"
+
+"""
+
+surprise = ["Groucho", "Chico", "Harpo"]
+
+print(surprise)
