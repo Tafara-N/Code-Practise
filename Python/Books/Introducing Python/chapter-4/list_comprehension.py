4c72d11e010522dfe9a6a14e283bf83b6bda3b3a
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-09-29 02:19:56 +0200

    A list comprehension for even numbers in range 10.

---
+++ Python/Books/Introducing Python/chapter-4/list_comprehension.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+Use a list comprehension to make a list of the `even` numbers in range(10)
+
+"""
+
+even = [number for number in range(10) if number % 2 == 0]
+
+print(even)
