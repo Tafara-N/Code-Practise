e1ce0f7cbbfe21d8738603f2ea3c28bdb690a571
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-09-29 18:59:01 +0200

    A function called good that returns the list.

---
+++ Python/Books/Introducing Python/chapter-4/return_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+A function called good that returns the list ['Harry', 'Ron', 'Hermione']
+
+"""
+
+
+def good() -> list[str]:
+    """
+    A function called good that returns the list ['Harry', 'Ron', 'Hermione']
+
+    Returns:
+        list[str]
+            Returns the list ['Harry', 'Ron', 'Hermione']
+    """
+
+    return ['Harry', 'Ron', 'Hermione']
+
+
+if __name__ == '__main__':
+    print(good())
