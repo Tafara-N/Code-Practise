72d055e542e7ec3d966aeb9386ba49f2e6b7eb35
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-10-03 22:35:30 +0200

    Print the string 'Open 9-5 daily'.

---
+++ Python/Books/Introducing Python/chapter-5/zoo.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+Create a file called `zoo.py`.
+
+In it, define a function called `hours()` that prints the string
+'Open 9-5 daily'.
+
+Then, use the interactive interpreter to import the zoo module and call its
+hours() function
+
+"""
+
+
+def hours() -> None:
+    """
+    Print the string 'Open 9-5 daily'
+    """
+
+    print("Open 9-5 daily")
+
+
+if __name__ == "__main__":
+    hours()
