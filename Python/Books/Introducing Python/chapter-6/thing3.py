2e919d251995264372a53cf71729fbb6c9ea3795
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-10-05 11:32:14 +0200

    A class called Thing3 with an init method.

---
+++ Python/Books/Introducing Python/chapter-6/thing3.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""
+Make yet another class called, of course, `Thing3`.
+
+This time, assign the value `'xyz'` to an instance (object) attribute called
+`letters`.
+
+Print `letters`.
+
+Do you need to make an object from the class to do this?
+
+"""
+
+class Thing3:
+    """
+    A Thing class
+    """
+
+    def __init__(self):
+        self.letters = 'xyz'
+
+thing = Thing3()
+print(thing.letters)
+
+# You need to make an object from the class to do this.
