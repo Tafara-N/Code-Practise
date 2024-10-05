863fa273d212c16a6480a4337cf5f43b95271926
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-10-05 11:28:01 +0200

    A class called Thing with class level variable.

---
+++ Python/Books/Introducing Python/chapter-6/thing2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+Make a new class called `Thing2` and assign the value 'abc' to a class
+attribute called `letters`.
+
+Print `letters`.
+
+"""
+
+class Thing2:
+    """
+    A class with no contents
+    """
+
+    letters = 'abc'
+
+print(Thing2.letters)
