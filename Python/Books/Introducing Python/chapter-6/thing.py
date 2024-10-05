a8a8c5d8e2eb24a513d1a3093b20f512dcdb48a7
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-10-05 11:23:19 +0200

    An empty class called Thing.

---
+++ Python/Books/Introducing Python/chapter-6/thing.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+Make a class called `Thing` with no contents and print it.
+
+Then, create an object called `example` from this class and also print it.
+
+Are the printed values the same or different?
+
+"""
+
+class Thing:
+    """
+    A class with no contents
+    """
+
+    pass
+
+print(Thing)
+
+example = Thing()
+print(example)
+
+# The printed values are different. The first one is the class itself, and the
+# second one is an object of the class.
