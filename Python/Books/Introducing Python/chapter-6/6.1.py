7357df0f1ba7dacfedaf8e385b7f011401054a82
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-10-10 18:00:28 +0200

    An empty class called 'Thing'.

--- Python/Books/Introducing Python/chapter-6/6.1.py
+++ Python/Books/Introducing Python/chapter-6/6.1.py
@@ -5,7 +5,7 @@ Make a class called `Thing` with no contents and print it.
 
 Then, create an object called `example` from this class and also print it.
 
-Are the printed values the same or different?
+Are the printed values the same or they are different?
 
 """
 
