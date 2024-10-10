16b795da2bd08158854cdd733fc7bf212e8da797
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-10-10 18:39:47 +0200

    Writingthe poem by using old-style formatting (%).

---
+++ Python/Books/Introducing Python/chapter-7/7.4.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+Write the poem by using old-style formatting.
+
+Substitute the strings: 'roast beef', 'ham', 'head', and 'clam'
+  into this string:
+
+"""
+
+poem = """    My kitty cat likes %s,
+    My kitty cat likes %s,
+    My kitty cat fell on his %s
+    And now thinks he's a %s.
+"""
+
+args = {
+    1: 'roast beef',
+    2: 'ham',
+    3: 'head',
+    4: 'clam'
+}
+
+print(poem % (args[1], args[2], args[3], args[4]), end='')
