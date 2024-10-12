c79d02fae0cff8e422591906bc1e24c398d63d47
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-10-12 02:32:11 +0200

    Create a database called 'books.db' with a 'books' table using sqlite3.

---
+++ Python/Books/Introducing Python/chapter-8/8.6.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+Use the sqlite3 module to create a SQLite database called books.db, and a table
+called books with these fields:
+  title (text), author (text), and year (integer)
+
+"""
+
+import sqlite3
+
+
+database = sqlite3.connect("books.db")
+
+query = """CREATE TABLE books (
+    title TEXT,
+    author TEXT,
+    year INT
+    )"""
+
+cursor = database.cursor()
+cursor.execute(query)
+database.commit()
+database.close()
