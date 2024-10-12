676129c0d39cc8c9379dfb8f00f9533a4f422ca2
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-10-12 17:28:12 +0200

    Read 'books.csv' and insert its data into the 'book' table.

--- Python/Books/Introducing Python/chapter-8/8.7.py
+++ Python/Books/Introducing Python/chapter-8/8.7.py
@@ -3,6 +3,10 @@
 """
 Read books.csv and insert its data into the book table.
 
+- Use the reader() method
+- convert the reader object to a list
+- slice the first line (Header Column) and load the data
+
 """
 
 import csv
@@ -11,13 +15,14 @@ import sqlite3
 database = sqlite3.connect("books.db")
 cursor = database.cursor()
 
-
 with open("books.csv", "r", newline="") as file:
-    data = csv.DictReader(file)
+    reader = csv.reader(file)
+    data = list(reader)
 
     query = "INSERT INTO books (title, author, year) VALUES (?, ?, ?)"
-    for row in data:
-        cursor.execute(query, (row['title'], row['author'], row['year']))
 
-database.commit()
-database.close()
+    for row in data[1:]:
+        cursor.execute(query, row)
+
+    database.commit()
+    database.close()
