0c5633f81e23717b4c6f760691c36c24987999e7
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-10-14 00:09:28 +0200

    Write the current date as a string to the text file today.txt

---
+++ Python/Books/Introducing Python/chapter-10/10.1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+Write the current date as a string to the text file today.txt
+
+"""
+
+import datetime
+
+
+def main() -> None:
+    """
+    Main function to write the current date to the file today.txt.
+    """
+
+    with open("today.txt", "wt") as file:
+        date = datetime.datetime.now().strftime("%Y-%m-%d")
+        file.write(date)
+
+
+if __name__ == "__main__":
+    main()
